dbf9aa1306414d2d3598606584bd27528173fea5
Artem Vanty <artem.vanty.0358@gmail.com>
2023-01-13 02:50:27 +0900

    test_saver

--- build.sh
+++ build.sh
@@ -1,49 +1,54 @@
 #!/bin/bash
 
 #PREPARING
+SRC_LIB_PATH="src/lib" # SOURCE LIB PATH
+SRC_TESTS_PATH="src/tests" # SOURCE TESTS PATH
 
-SRC_LIB_PATH="src/lib"
-SRC_TESTS_PATH="src/tests"
-
-DST_LIB_PATH="build/lib"
-DST_TESTS_PATH="build/tests"
+DST_LIB_PATH="build/lib" # LIB PATH DESTINATION
+DST_TESTS_PATH="build/tests" # TESTS PATH DESTINATION
 
 LIBS=$(ls src/lib); # GETTING LIBS IN *.h FORMAT
 TESTS=$(ls src/tests) # GETTING TESTS IN *.cpp FORMAT
 
-#CHECKING DIRS
+function check_dirs()
+{
+    # CHECKING DIRS
+    if [ ! -d "build" ]; then mkdir build; fi # CHECKING build FOLDER
+    if [ ! -d "build/tests" ]; then mkdir build/tests; fi # CHECKING build/tests FOLDER
+    if [ ! -d "build/lib" ]; then mkdir build/lib; fi # CHECKING build/lib FOLDER
+    if [ ! -d "build/lib/so" ]; then mkdir build/lib/so; fi # CHECKING build/lib/so FOLDER
+    if [ ! -d "build/lib/a" ]; then mkdir build/lib/a; fi # CHECKING build/lib/a FOLDER
+}
 
-if [ ! -d "build" ]; then mkdir build; fi
-if [ ! -d "build/tests" ]; then mkdir build/tests; fi
-if [ ! -d "build/lib" ]; then mkdir build/lib; fi
-if [ ! -d "build/lib/so" ]; then mkdir build/lib/so; fi
-if [ ! -d "build/lib/a" ]; then mkdir build/lib/a; fi
+function compile_tests()
+{
+    # COMPILING TESTS
+    for FILE in $TESTS;
+    do
+        echo building "$SRC_TESTS_PATH/$FILE"
+        clang++\
+            -std=c++20\
+            -O3\
+            "$SRC_TESTS_PATH/$FILE"\
+            -o\
+            "$DST_TESTS_PATH/$(basename "$FILE" .cpp)"\
+            && printf "\e[01;38;05;10m%s\e[0m\n\n" "Success Compiling!"\
+            || printf "\e[01;38;05;1m%s\e[0m\n\n" "An Errors Occurred!"
+    done
+}
 
-for i in $TESTS; do
-    time\
-    clang++\
-    -std=c++20\
-    -O3\
-    $SRC_TESTS_PATH/$TESTS\
-    -o\
-    $DST_TESTS_PATH/$TESTS\
-    || printf "\e[01;38;05;1m%s\n\n" "Errors!"\
-    || printf "\e[01;38;05;10m%s\n\n" "Success Compiling!";
-done
+function main() 
+{
+    check_dirs; # CHECKING DIRS
 
-exit 0;
+    compile_tests; # COMPILING TESTS
 
-#COMPILING TESTS
-time\
-    clang++\
-    -std=c++20\
-    -O3\
-    $tests\
-    -o ../../build/tests/\
-    || printf "\e[01;38;05;1m%s\n\n" "Errors!"\
-    || printf "\e[01;38;05;10m%s\n\n" "Success Compiling!";
+    # COMPILING LIBS
 
-#COMPILING LIBS
-    #COMPILING .so FILES
+        # COMPILING .so FILES
 
-    #COMPILING .a FILES
+
+        # COMPILING .a FILES
+}
+
+time main;
